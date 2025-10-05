35cd593f58176fc6a482cab26af3a075d754a72f
Anas Tabba <anastabba@Anass-MacBook-Air.local>
2025-10-05 16:37:04 +0500

    Lab: time + answers + interpose/sandbox/attack changes

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,3 +1,5 @@
+
+#include "param.h"
 // Saved registers for kernel context switches.
 struct context {
   uint64 ra;
@@ -104,4 +106,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint64 deny_mask;            // This pathname is always denied
+  char allow_path[MAXPATH];    // This pathname is always allowed for some reason
 };
